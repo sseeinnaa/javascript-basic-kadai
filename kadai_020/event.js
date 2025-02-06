6771f732ec29845e233332c9ca7fbc2202d2e5c9
sseeinnaa <seina9655@icloud.com>
2025-02-06 17:45:47 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+const Btn = document.getElementById('btn');
+const h2 = document.getElementById('text');
+Btn.addEventListener('click',()=>{
+    h2.textContent="ボタンをクリックしました";
+});
+
+
+    
+    
+
